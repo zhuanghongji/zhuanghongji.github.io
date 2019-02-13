9ce7bc04cd3ce6b8dfb9e20dcced9e2d71010eb5
zhuanghongji <zhuanghongjichina@gmail.com>
2019-02-13 22:18:45 +0800

    First Release

---
+++ release/precache-manifest.a5d820e9271ce2497591c0472d738992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfc3ee7e9981bfea2186",
+    "url": "./static/css/main.f2e9b659.chunk.css"
+  },
+  {
+    "revision": "bfc3ee7e9981bfea2186",
+    "url": "./static/js/main.bfc3ee7e.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "./static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "52ee8a1540ba4f55c126d54141f8bd20",
+    "url": "./static/media/avatar.52ee8a15.jpg"
+  },
+  {
+    "revision": "31b98d184c192f3aff89afb94363637e",
+    "url": "./index.html"
+  }
+];
