317fd1967eb827bd6a6b63351d5b3c11a8294d10
zhuanghongji <zhuanghongji.com@gmail.com>
2019-07-29 16:08:12 +0800

    Add profile production.

---
+++ profile/precache-manifest.ae57d9077a0d376ece3c3ed545dc028f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1bbb1970625fbf9e94ce9787ba3018",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3976c160fd3795d6e3e5",
+    "url": "./static/css/main.a2e7789d.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "./static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3976c160fd3795d6e3e5",
+    "url": "./static/js/main.2a1dae31.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a57de1dcd747cf5b8fbbd851bfd6a1b4",
+    "url": "./static/media/avatar.a57de1dc.jpeg"
+  }
+]);
